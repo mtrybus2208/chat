b6f7aeb61c6e9717bcb254fa9e128411b3ced9aa
mtrybus2208 <mtrybus2208@wp.pl>
2020-08-14 15:54:37 +0200

    test

--- src/lib/helpers/databaseConnection.ts
+++ src/lib/helpers/databaseConnection.ts
@@ -8,6 +8,14 @@ export class DatabaseConnection {
     const migrations: string[] = config.get('typeorm.migrations');
     const subscribers: string[] = config.get('typeorm.subscribers');
 
+    console.log({
+      host: env.get('DB_HOST').asString(),
+      port: env.get('DB_PORT').asPortNumber(),
+      database: env.get('DB_NAME').asString(),
+      username: env.get('DB_USER').asString(),
+      password: env.get('DB_PASS').asString(),
+    });
+
     return createConnection({
       type: 'postgres',
       host: env.get('DB_HOST').asString(),
